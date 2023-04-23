e1a98946168c26bf7a7723c48e9fcc1cd8985d5a
Demiourg <george.alexiou@tutanota.com>
2023-04-23 16:49:46 +0200

    add tryouts for headline positioning

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <main>
-      <h1>
-        Farmtrade <span>Support your local farmers</span>
-      </h1>
-      <h2 className="h2-home">Eat local, seasonal & sustainable</h2>
+    <main className="home-main">
+      <section className="header-section">
+        <h1>
+          Farmtrade
+          <span className="span-home">Support your local farmers</span>
+        </h1>
+        <h2 className="h2-home">Eat local, seasonal & sustainable</h2>
+      </section>
     </main>
   );
 };
