7757fd298b4b1ee9741a0da035d3ddffc3864d09
Demiourg <george.alexiou@tutanota.com>
2023-04-24 14:09:46 +0200

    add text

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,29 +2,29 @@ import "./Nav.css";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-	return (
-		<header>
-			<nav>
-				<ul className="nav-positioning">
-					<li className="nav-item">
-						<Link to={"/"}>Home</Link>
-					</li>
-					<li className="nav-item">
-						<Link to={"/events"}>Events</Link>
-					</li>
-					<li className="nav-item">
-						<Link to={"/freshboard"}>Fresh Board</Link>
-					</li>
-					<li className="nav-item">
-						<Link to={"/whatsnew"}>What`s new</Link>
-					</li>
-					<li className="nav-item">
-						<Link to={"/profile"}>Profile</Link>
-					</li>
-				</ul>
-			</nav>
-		</header>
-	);
+  return (
+    <header>
+      <nav>
+        <ul className="nav-positioning">
+          <li className="nav-item">
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/events"}>Events</Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/freshboard"}>Fresh Board</Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/whatsnew"}>What`s new</Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/profile"}>Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
--- src/pages/Freshboard/Freshboard.js
+++ src/pages/Freshboard/Freshboard.js
@@ -1,7 +1,11 @@
 import "./Freshboard.css";
 
 const Freshboard = () => {
-  return <h2>Freshboard</h2>;
+  return (
+  <div> 
+  <h2>Freshboard</h2>
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit vitae et rem quae esse placeat itaque officiis nisi delectus dignissimos nesciunt perferendis magni ullam molestias enim laborum, aut, sit iste!</p>);
+  </div>
 };
 
 export default Freshboard;
