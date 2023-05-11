d78bc7ab68b094cedb38d18ff56f6b7ad8997509
Jai Bheeman <jai.bheeman@gmail.com>
2023-05-11 17:26:42 +0200

    fix: add ;

--- src/pages/Freshboard/Freshboard.js
+++ src/pages/Freshboard/Freshboard.js
@@ -1,11 +1,14 @@
 import "./Freshboard.css";
 
 const Freshboard = () => {
-  return (
-  <div> 
-  <h2>Freshboard</h2>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit vitae et rem quae esse placeat itaque officiis nisi delectus dignissimos nesciunt perferendis magni ullam molestias enim laborum, aut, sit iste!</p>);
-  </div>
+    return (
+        <div>
+            <h2>Freshboard</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit vitae et rem quae esse placeat itaque
+                officiis nisi delectus dignissimos nesciunt perferendis magni ullam molestias enim laborum, aut, sit
+                iste!</p>);
+        </div>
+    );
 };
 
 export default Freshboard;
